7d206ddadf06095cc5231370d1bdb4248952cc87
ericvyc <eric_vilar2007@hotmail.com>
2016-06-10 09:48:57 -0300

    EntityManager único.

--- autoinstrucional/src/main/java/br/com/fumec/dao/impl/BaseDAO.java
+++ autoinstrucional/src/main/java/br/com/fumec/dao/impl/BaseDAO.java
@@ -9,11 +9,23 @@ import br.com.fumec.dao.IBaseDAO;
 public class BaseDAO implements IBaseDAO {
 
 	private static final long serialVersionUID = -8903543658011390617L;
+	
+	private EntityManagerFactory emf;
+	
+	private EntityManager em;
 
 	@Override
 	public EntityManager getEntityManager(){
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("autoinstrucional");
-		return emf.createEntityManager();
+		
+		if(emf == null){
+			emf= Persistence.createEntityManagerFactory("autoinstrucional");
+		}
+		if(em == null) {
+			em = emf.createEntityManager();
+		}
+		
+		return em;
+		
 	}
 
 }
